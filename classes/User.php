4cac9ddeaf9f28e8d25e97b701a04391c7c06294
britt <britt110100@gmail.com>
2020-03-19 18:23:11 +0100

    Added basic from to register.php
    made classes Db & User
    
    Able to commit into github
    
    next: hash password, email moet eindigen op..., email mag nog niet bestaan

---
+++ classes/User.php
@@ -0,0 +1,132 @@
+<?php
+
+include_once(__DIR__ . "/Db.php");
+    class User{
+        private $firstname;
+        private $lastname;
+        private $email;
+        private $password;
+
+        /**
+         * Get the value of firstname
+         */ 
+        public function getFirstname()
+        {
+                return $this->firstname;
+        }
+
+        /**
+         * Set the value of firstname
+         *
+         * @return  self
+         */ 
+        public function setFirstname($firstname)
+        {
+            if(empty($firstname)){
+                throw new Exception("firsname cannot be empty");
+            }
+                $this->firstname = $firstname;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of lastname
+         */ 
+        public function getLastname()
+        {
+                return $this->lastname;
+        }
+
+        /**
+         * Set the value of lastname
+         *
+         * @return  self
+         */ 
+        public function setLastname($lastname)
+        {
+            if(empty($lastname)){
+                throw new Exception("lastname cannot be empty");
+            }
+                $this->lastname = $lastname;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of email
+         */ 
+        public function getEmail()
+        {
+                return $this->email;
+        }
+
+        /**
+         * Set the value of email
+         *
+         * @return  self
+         */ 
+        public function setEmail($email)
+        {
+            if(empty($email)){
+                throw new Exception("email cannot be empty");
+            }
+                $this->email = $email;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of password
+         */ 
+        public function getPassword()
+        {
+                return $this->password;
+        }
+
+        /**
+         * Set the value of password
+         *
+         * @return  self
+         */ 
+        public function setPassword($password)
+        {
+            if(empty($password)){
+                throw new Exception("password cannot be empty");
+            }
+                $this->password = $password;
+
+                return $this;
+        }
+
+
+        public function save(){
+            $conn = Db::getConnection();
+
+            $statement = $conn->prepare("insert into user (firstname, lastname, email, password) values (:firstname, :lastname, :email, :password)");
+
+            $firstname = $this->getFirstname();
+            $lastname = $this->getLastname();
+            $email = $this->getEmail();
+            $password = $this->getPassword();
+
+            $statement->bindValue(":firstname", $firstname);
+            $statement->bindValue(":lastname", $lastname);
+            $statement->bindValue(":email", $email);
+            $statement->bindValue(":password", $password);
+
+            $result = $statement->execute();
+
+            return $result;
+        }
+
+        public static function getAll(){
+            $conn = Db::getConnection();
+
+            $statement = $conn->prepare("select * from user");
+            $statement->execute();
+            $users = $statement->fetchAll(PDO::FETCH_ASSOC);
+            return $users;
+        }
+    }
+?>
---
+++ register.php
@@ -0,0 +1,77 @@
+<?php
+include_once(__DIR__ ."/classes/User.php");
+    if(!empty($_POST)){
+
+        try {
+            $user = new User();
+            $user->setFirstname($_POST['firstname']);
+            $user->setLastname($_POST['lastname']);
+            $user->setEmail($_POST['email']);
+            $user->setPassword($_POST['password']);
+
+            $user->save();
+            $succes = "User saved";
+
+            header("Location: index.php");
+        } catch (\Throwable $th){
+            $error = $th->getMessage();
+        }
+    }
+
+    $users = User::getAll();
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" href="css/register.css">
+</head>
+<body>
+    <?php if(isset($error)): ?> 
+        <div class="error" style="color: red;"><?php echo $error; ?></div>
+    <?php endif; ?>
+
+    <?php if(isset($error)): ?>
+        <div class="succes" style="color: green;"><?php echo $succes; ?></div>
+    <?php endif; ?>
+    <div class="container register-form">
+        <div class="form">
+            <div class="note">
+                <p>Sign up voor de IMD buddy app.</p>
+            </div>
+
+            <form action="" method="POST">
+                <div class="form-content">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <input name="firstname" id="firstname" type="text" class="form-control" placeholder="Your Name *" value=""/>
+                            </div>
+                            <div class="form-group">
+                                <input name="lastname" id="lastname" type="text" class="form-control" placeholder="Your lastname *" value=""/>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <input name="email" id="email" type="text" class="form-control" placeholder="Your email *" value=""/>
+                            </div>
+                            <div class="form-group">
+                                <input name="password" id="password" type="text" class="form-control" placeholder="Your Password *" value=""/>
+                            </div>
+                        </div>
+                    </div>
+                    <button type="submit" class="btnSubmit">Sign me up</button>
+                </div>
+                </form>
+            </div>
+        </div>
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
