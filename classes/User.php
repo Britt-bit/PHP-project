f0b83a885f68e2261915e828996cbd0a71effa60
Koen11 <r0718185@tudent.thomasmore.be>
2020-03-29 16:02:43 +0200

    Feature 4

---
+++ classes/User.php
@@ -0,0 +1,282 @@
+<?php
+
+include_once(__DIR__ . "/Db.php");
+    class User{
+        private $firstname;
+        private $lastname;
+        private $email;
+        private $avatar;
+        private $bio;
+        private $password;
+        private $newpassword;
+        
+        
+        
+
+        /**
+         * Get the value of firstname
+         */ 
+        public function getFirstname()
+        {
+                return $this->firstname;
+        }
+
+        /**
+         * Set the value of firstname
+         *
+         * @return  self
+         */ 
+        public function setFirstname($firstname)
+        {
+            if(empty($firstname)){
+                throw new Exception("Firsname cannot be empty");
+            }
+                $this->firstname = $firstname;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of lastname
+         */ 
+        public function getLastname()
+        {
+                return $this->lastname;
+        }
+
+        /**
+         * Set the value of lastname
+         *
+         * @return  self
+         */ 
+        public function setLastname($lastname)
+        {
+            if(empty($lastname)){
+                throw new Exception("Lastname cannot be empty");
+            }
+                $this->lastname = $lastname;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of email
+         */ 
+        public function getEmail()
+        {
+                return $this->email;
+        }
+
+        /**
+         * Set the value of email
+         *
+         * @return  self
+         */ 
+
+        public function setEmail($email)
+        {
+
+
+            if(empty($email)){
+                throw new Exception("Email cannot be empty");
+            } 
+                
+                $this->email = $email;
+                return $this;
+               
+        }
+        
+        /**
+         * Get the value of avatar
+         */ 
+        public function getAvatar()
+        {
+                return $this->avatar;
+        }
+
+        /**
+         * Set the value of avatar
+         *
+         * @return  self
+         */ 
+        public function setAvatar($avatar)
+        {
+                $this->avatar = $avatar;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of bio
+         */ 
+        public function getBio()
+        {
+                return $this->bio;
+        }
+
+        /**
+         * Set the value of bio
+         *
+         * @return  self
+         */ 
+        public function setBio($bio)
+        {
+                $this->bio = $bio;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of password
+         */ 
+        public function getPassword()
+        {
+                return $this->password;
+        }
+
+        /**
+         * Set the value of password
+         *
+         * @return  self
+         */ 
+        public function setPassword($password)
+        {
+            if(empty($password)){
+                throw new Exception("Password cannot be empty");
+            }
+                $this->password = $password;
+
+                return $this;
+        }
+
+         /**
+         * Get the value of newpassword
+         */ 
+        public function getNewpassword()
+        {
+                return $this->newpassword;
+        }
+
+        /**
+         * Set the value of newpassword
+         *
+         * @return  self
+         */ 
+        public function setNewpassword($newpassword)
+        {
+                $this->newpassword = $newpassword;
+
+                return $this;
+        }
+
+        public function save(){
+            $conn = Db::getConnection();
+
+            $statement = $conn->prepare("insert into user (firstname, lastname, email, password) values (:firstname, :lastname, :email, :password)");
+
+            $firstname = $this->getFirstname();
+            $lastname = $this->getLastname();
+            $email = $this->getEmail();
+            $password = $this->getPassword();
+
+            $statement->bindValue(":firstname", $firstname);
+            $statement->bindValue(":lastname", $lastname);
+            $statement->bindValue(":email", $email);
+            $statement->bindValue(":password", $password);
+
+            $result = $statement->execute();
+
+            return $result;
+        }
+
+        
+
+        public static function getAll(){
+            $conn = Db::getConnection();
+            
+            $statement = $conn->prepare("select * from user");
+            $statement->execute();
+            $users = $statement->fetchAll(PDO::FETCH_ASSOC);
+            return $users;
+        }
+
+        public function myemail(){
+            $email = $this->getEmail();
+            $conn = Db::getConnection();
+
+            $check_email = ("SELECT email FROM user WHERE email='$email'");
+
+            foreach ($conn->query($check_email) as $row){
+                print $row['email'];
+            }
+            return $row;
+        }
+        /* update user */
+        function getUserById($id){
+            $conn = Db::getConnection();
+            $statement = $conn->prepare('select * from user where id = :id');
+            $statement->bindParam(':id', $id);
+            $statement->execute();
+            $result = $statement->fetch();
+            return $result;
+        }
+
+
+
+        function updateUser()
+        {
+               if (move_uploaded_file($_FILES["avatar"]["tmp_name"], $this->avatar)) {
+                    $conn = Db::getConnection();
+                    $statement = $conn->prepare("update user set firstname= :firstname, lastname= :lastname, email= :email, avatar= :avatar, bio= :bio");
+                    $statement->bindParam(":firstname", $this->firstname);
+                    $statement->bindParam(":lastname", $this->lastname);
+                    $statement->bindParam(":email", $this->email);
+                    $statement->bindParam(":avatar", $this->avatar);
+                    $statement->bindParam(":bio", $this->bio);
+
+                    $statement->execute();
+               }else {
+                    $conn = Db::getConnection();
+                    $statement = $conn->prepare("update user set firstname= :firstname, lastname= :lastname, email= :email, bio= :bio");
+                    $statement->bindParam(":firstname", $this->firstname);
+                    $statement->bindParam(":lastname", $this->lastname);
+                    $statement->bindParam(":email", $this->email);
+                    $statement->bindParam(":bio", $this->bio);
+
+                    $statement->execute();
+               }
+            
+        }
+        function updatePassword()
+        {
+            
+            $conn = Db::getConnection();
+            $statement = $conn->prepare("update user set password= :password");
+            $statement->bindParam(":password", $this->newpassword);
+
+            $statement->execute();
+        }
+        function passwordCheck($id, $password)
+        {
+            $user = self::getUserById($id);
+
+            if ($password == $user['password']) {
+                return true;
+            }
+            else{
+                return false;
+            }
+            
+        }
+        function passwordHash($password)
+        {
+            $options = [
+                'cost' => 12,
+            ];
+            $hash = password_hash($password, PASSWORD_DEFAULT, $options);
+
+            return $hash;
+        }
+        
+
+    }
---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+//klasse en database copy pasten naar hier
+include_once(__DIR__ . "/classes/User.php");
+include_once(__DIR__ . "/classes/Db.php");
+include_once(__DIR__ ."/classes/Features.class.php");
+
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP project</title>
+</head>
+<body>
+    <h1>Logged in</h1>
+    <a class="nav-link" href="profile.php?id=<?php $_SESSION['user'][0] ?>">Profile</a>
+
+<?php 
+
+
+//check of user features al ingevuld heeft. zoniet->redirect naar invulformulier
+$checkFeatures = feature::checkFeatures();
+
+if ($checkFeatures == false){
+    header("Location: features.php");
+    exit();
+   }
+
+// laten zien wanneer hobby nog niet is ingevuld 
+$hobby = feature::hobby();
+
+if(empty($hobby)){
+    include_once(__DIR__ ."/completeFeatures.php");
+}  
+
+?>
+
+
+</body>
+</html>
