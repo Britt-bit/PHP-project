a908187f4fcb0329c7917b346c65241cb14ecf0e
Koen11 <49272473+Koen11@users.noreply.github.com>
2020-04-05 21:38:42 +0200

    eigen profiel niet tonen

--- classes/Post.php
+++ classes/Post.php
@@ -4,9 +4,14 @@ class Post{
     public static function search($search)
     {
 
-        
         $conn = Db::getConnection();
-        $statement = $conn->prepare("SELECT firstname, lastname, avatar, games, films, muziek, vak, hobby FROM user
+
+        $statement = $conn->prepare("SELECT user_id FROM user WHERE email = '".$_SESSION['email']."'");
+        $statement->execute();
+        $id = $statement->fetch(PDO::FETCH_COLUMN);
+        
+        
+        $statement = $conn->prepare("SELECT firstname, lastname, avatar, email, games, films, muziek, vak, hobby FROM user
         INNER JOIN features on user.user_id=features.user_id WHERE 
         firstname like :search 
         OR lastname like :search 
@@ -15,8 +20,9 @@ class Post{
         OR muziek like :search
         OR vak like :search
         OR hobby like :search
+        AND user.user_id != :id 
         ");
-
+        $statement->bindValue(":id", $id);
         $statement->bindValue(":search", $search);
         $statement->execute();
         $search = $statement->fetchAll();
@@ -27,4 +33,3 @@ class Post{
 }
 
 ?>
-
