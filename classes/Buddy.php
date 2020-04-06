0a6cf4e28713026f872b2daf083639013960fd69
r0739428 <tazi.maryam@hotmail.com>
2020-04-06 21:59:42 +0200

    Buddy
    Buddies in een lijstje krijgen. Nog niet werkende...

---
+++ classes/Buddy.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once(__DIR__ . "/Db.php");
+include_once(__DIR__ . "/classes/Db.php");
+include_once(__DIR__ ."/classes/Match.php");
+
+
+//Als je een buddy hebt --> opslaan in de database
+
+//connectie met databank
+
+//query toevoegen
+
+//value binden
+
+
+
+//Buddies uit databank halen
+
+?>
