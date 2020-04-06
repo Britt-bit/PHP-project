e41bfca31770651bfafbeeddb7c3b3a5e68f2220
Britt-bit <55791941+Britt-bit@users.noreply.github.com>
2020-04-06 10:35:26 +0200

    Update match.php

--- match.php
+++ match.php
@@ -1,15 +1,12 @@
 <?php
 session_start();
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
 //klasse en database copy pasten naar hier
 include_once(__DIR__ . "/classes/User.php");
 include_once(__DIR__ . "/classes/Db.php");
 include_once(__DIR__ ."/classes/Features.class.php");
 include_once(__DIR__ ."/classes/Match.php");
-
-
-//error_reporting(E_ALL);
-//ini_set('display_errors', 1);
-
 
 if(isset($_GET['chat'])){
     $id = $_GET['chat'];
@@ -17,12 +14,9 @@ if(isset($_GET['chat'])){
     $result = $mysqli->query("SELECT * FROM user WHERE user_id=$id") or die($mysqli->error());
     if (count($result) ==1){
         $row = $result->fetch_array();
-        var_dump($id);
-        //var_dump($result);
+        //var_dump($id);
     }
 }
-
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -35,7 +29,6 @@ if(isset($_GET['chat'])){
 
     <h1>My matches</h1>
     <form method="post">
-  
     <table>
         <tr>
             <!-- Alle mogelijke matches oplijsten
@@ -45,10 +38,6 @@ if(isset($_GET['chat'])){
         while($yourFeature = $statement->fetch( PDO::FETCH_ASSOC )){ 
             //loopen over alle mogelijke gebruikers in de database
             for ($counter = 0; $yourFeature = $statement->fetch( PDO::FETCH_ASSOC ); ++$counter){
-                
-                
-         
-
                 $yourGame = $yourFeature['games']; 
                 $yourFilm = $yourFeature['films'];
                 $yourMusic = $yourFeature['muziek']; 
@@ -62,66 +51,53 @@ if(isset($_GET['chat'])){
                 $stmt->execute();
                 $id = $stmt->fetch(PDO::FETCH_COLUMN);
 
-               
-      
                 //al deze features in een 2de array zetten
-            $yourFeatureArray = array($yourGame, $yourFilm, $yourMusic, $yourCourse, $yourHobby);
+                $yourFeatureArray = array($yourGame, $yourFilm, $yourMusic, $yourCourse, $yourHobby);
 
-            //De 2 arrays vergelijken om te zien welke features allemaal matchen.
-            $result = array_intersect($myFeatures, $yourFeatureArray);
+                //De 2 arrays vergelijken om te zien welke features allemaal matchen.
+                $result = array_intersect($myFeatures, $yourFeatureArray);
             
-            //als er 5 dezelfde features zijn ... 
-            if(count($result) === 5){
-                echo "<br/>";
-                print("You matched with " . $yourName . " " . $yourLastname . " on the features "); 
-                 for($tel = 0; $tel < sizeof($result); ++$tel){
-                    if($tel < sizeof($result) -1){
-                        echo($result[$tel] . ", ");
-                    } else {
-                        echo($result[$tel] . ".");
+                //als er 5 dezelfde features zijn ... 
+                if(count($result) === 5){
+                    echo "<br/>";
+                    print("You matched with " . $yourName . " " . $yourLastname . " on the features "); 
+                        for($tel = 0; $tel < sizeof($result); ++$tel){
+                            if($tel < sizeof($result) -1){
+                                echo($result[$tel] . ", ");
+                            } else {
+                                echo($result[$tel] . ".");
+                            }
+                        }
+                    echo '<a href="chat.php?chat=' . $id . '">Chat</a>'
+                ?>
+            </tr>
+            <?php
+                }else if(count($result) === 4){
+                    echo "<br/>";
+                    print("You matched with " . $yourName . " " . $yourLastname . " on the features "); 
+                    for($tel = 0; $tel < sizeof($result) +2; ++$tel){
+                        echo($result[$tel] . "  ");
                     }
-                }
                 echo '<a href="chat.php?chat=' . $id . '">Chat</a>'
-                ?>
+            ?>
             </tr>
-           
             <?php
-            } else if(count($result) === 4){
-                echo "<br/>";
-                print("You matched with " . $yourName . " " . $yourLastname . " on the features "); 
-                 for($tel = 0; $tel < sizeof($result) +2; ++$tel){
-                        echo($result[$tel] . "  ");
-                    
-                }
+                } else if(count($result) === 3){
+                    echo "<br/>";
+                    print("You matched with " . $yourName . " " . $yourLastname . " on the features "); 
+                    for($tel = 0; $tel < sizeof($result) +2; ++$tel){
+                        echo($result[$tel] . "  ");   
+                    }
                 echo '<a href="chat.php?chat=' . $id . '">Chat</a>'
-                ?>
+            ?>
             </tr>
-           
-            <?php
-            } else if(count($result) === 3){
-                echo "<br/>";
-                print("You matched with " . $yourName . " " . $yourLastname . " on the features "); 
-                 for($tel = 0; $tel < sizeof($result) +2; ++$tel){
-                        echo($result[$tel] . "  ");
-                        
-                }
-                //echo '<input type="submit" id="chat" name="chat[]" value="'. $row['chat'] .'">';
-                echo '<a href="chat.php?chat=' . $id . '">Chat</a>'
-                ?>
-            </tr>
-      
             <?php
             }
-
-    
         }
     }
-    ?>
-   
-        
-    </table>
-    </form>
-   
- 
+?>
+</table>
+</form>
+
 </body>
 </html>
