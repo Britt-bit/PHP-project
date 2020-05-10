f01f045992d1db2fd133a288c2e364640be7bbba
MylanDeSaeger <33631464+MylanDeSaeger@users.noreply.github.com>
2020-05-10 12:21:16 +0200

    added mailing but in comment

--- ajax/sendBuddyRequest.php
+++ ajax/sendBuddyRequest.php
@@ -1,4 +1,11 @@
 <?php
+
+    /* use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'vendor/autoload.php'; */
+
     include_once '../classes/Request.php';
     include_once '../classes/Db.php';
     $id = $_POST['id'];
@@ -8,6 +15,49 @@
     $response = [];
     if (Request::sendRequest($id, $uid, $email, $person)) {
         $response['status'] = 'success';
+
+        /* //send email
+
+        $mail = new PHPMailer(true);
+
+        try {
+            //Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+            $mail->isSMTP();                                            // Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+            $mail->Username   = 'Buddiez.PHP@gmail.com';                     // SMTP username
+            $mail->Password   = '4@1dgbo(w@93G8B';                               // SMTP password
+            //$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+            //$mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+            $mail->Port       = 465;
+            $mail->SMTPSecure = "ssl";
+
+            //Recipients
+            $mail->setFrom('Buddiez.PHP@gmail.com', 'Buddiez team');
+            $mail->addAddress($email);     // Add a recipient
+
+            $mail->smtpConnect(
+                array(
+                    "ssl" => array(
+                        "verify_peer" => false,
+                        "verify_peer_name" => false,
+                        "allow_self_signed" => true
+                    )
+                )
+            );
+
+            // Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'New Buddy request';
+            $mail->Body    = 'You have a request from'. $person . '.';
+            //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+            $mail->send();
+            return true;
+        } catch (Exception $e) {
+            return false;
+        } */
     } else {
         $response['status'] = 'something went wrong.';
     }
--- classes/Request.php
+++ classes/Request.php
@@ -1,12 +1,6 @@
 <?php
 include_once(__DIR__ . "/Db.php");
 
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
-require 'vendor/autoload.php';
 class Request
 {
 
@@ -35,40 +29,6 @@ class Request
             $statement->bindParam(':buddy_id', $id);
             $statement->bindParam(':seeker_id', $uid);
             $statement->execute();
-
-            //send email
-
-            $mail = new PHPMailer(true);
-
-            try {
-                //Server settings
-                $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-                $mail->isSMTP();                                            // Send using SMTP
-                $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-                $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-                $mail->Username   = 'Buddiez.PHP@gmail.com';                     // SMTP username
-                $mail->Password   = '4@1dgbo(w@93G8B';                               // SMTP password
-                //$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-                //$mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-                $mail->Port       = 465;
-                $mail->SMTPSecure = "ssl";
-
-                //Recipients
-                $mail->setFrom('Buddiez.PHP@gmail.com', 'Buddiez team');
-                $mail->addAddress($email);     // Add a recipient
-
-                // Content
-                $mail->isHTML(true);                                  // Set email format to HTML
-                $mail->Subject = 'New Buddy request';
-                $mail->Body    = 'You have a request from'. $person . '.';
-                //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-                $mail->send();
-                return true;
-            } catch (Exception $e) {
-                return false;
-            }
-
 
             return true;
         } catch (\Throwable $th) {
